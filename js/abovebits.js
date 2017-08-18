ab044548157f22ffcdd6e77a09cbe75e07a0d312
LBI <lbitestme@gmail.com>
2017-08-18 19:01:31 +0300

    new design

---
+++ js/abovebits.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+	 // executes when complete page is fully loaded, including all frames, objects and images
+	var owl = jQuery('.owl-carousel').owlCarousel({
+		loop:true,
+		autoplay: 2000,
+		autoplaySpeed: 3000,
+		autoWidth:true
+	});
+});
