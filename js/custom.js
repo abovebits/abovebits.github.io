7ffde8a959dc3ed51ae43b930a36a11ea982cf57
Artem Kh <artem@abovebits.com>
2019-01-16 16:43:38 +0200

    Fixes

--- js/custom.js
+++ js/custom.js
@@ -60,3 +60,15 @@ function validateContact() {
 	
 	return valid;
 }
+/**Header progress bar**/
+$(window).load(function(){
+    $(window).scroll(function() {
+        var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
+        console.log(wintop);
+        var totalScroll = (wintop/(docheight-winheight))*100;
+        console.log("total scroll" + totalScroll);
+        $(".KW_progressBar").css("width",totalScroll+"%");
+    });
+
+});
+/**End of Header progress bar**/
