bb5d87ab5453d57debdd37fbbdb12365e783046c
diedov <diedov@abovebits.com>
2020-01-03 17:38:34 +0200

    fixed bug scroll skills

--- js/abovebits.js
+++ js/abovebits.js
@@ -108,7 +108,7 @@ var MarkersSwitcher = Object.create(function () {
 	}
 }());
 $(window).on('resize', function(){
-	calculateHeight();
+	//calculateHeight();
 });
 
 $(document).ready( function() {
@@ -411,7 +411,7 @@ function updateLayout() {
 	});
 }
 function isCollapse() {
-	var $stateBtn = $("body a[data-state]"),
+	var $stateBtn = $("body #skills a[data-state]"),
 		$gallery = $('body #skills .container_gallery');
 
 	if ($stateBtn.attr('data-state') === 'expand') {
