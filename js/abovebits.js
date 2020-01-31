5851f0d7dc55a0a1e15a67dab05c5893f4edace8
diedov <diedov@abovebits.com>
2020-01-31 11:03:15 +0200

    fix logo

--- js/abovebits.js
+++ js/abovebits.js
@@ -125,27 +125,23 @@ $(document).ready( function() {
 	var macintosh = /Macintosh/i.test(navigator.userAgent);
 
 	if (!isMobile && !is_ie) {
-		//console.log('is_ie  '+is_ie);
 		$('#top-parallax').parallax("10%", 0.3);
 		$('#responsive').parallax("10%", 0.2);
 		$('#contact').parallax("10%", 0.1);
 		$('.parallax').css({'background-attachment' : 'fixed'});
 	}
 	if (isiOs) {
-		//console.log('isiOs  '+isiOs);
 		$('#top-parallax').css({'background' : 'url( "../video/Working-Space_crop.jpg"  ) no-repeat top center fixed', 'background-size':'auto 100vmax'});
 		$('#responsive').css({'background' : 'url( "../images/responsive/office-space.jpg") 100%'});
 		$('#contact').css({'background' : 'url("../images/bg_contacts_more.png") #21c967'});
 	} else if (macintosh) {
-		//console.log('macintosh  '+macintosh);
 		var supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
 		if (supportsTouch) {
 			$('#top-parallax').css({'background' : 'url( "../video/Working-Space_crop.jpg"  ) no-repeat top center fixed', 'background-size':'auto 120vmax'});
-			$('#top-parallax img.magento_logo').css({'height':'30% !important'});
-			//console.log($('#top-parallax .magento_logo'));
+			$('#top-parallax img.magento_logo').addClass('tablet-height');
 		} else {
 			$('#top-parallax').css({'background' : 'url( "../video/Working-Space_crop.jpg"  ) no-repeat top center fixed', 'background-size':'auto 75vmax'});
-			$('#top-parallax img.magento_logo').css({'height':'8% !important'});
+			$('#top-parallax img.magento_logo').addClass('huge-desktop-height');
 		}
 		$('#responsive').css({
 			'background' : 'url( "../images/responsive/office-space.jpg")',
